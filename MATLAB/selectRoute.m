5d2762dc6b432df526df48f0384f3075497010b5
HimpeWillem <willem.himpe@gmail.com>
2018-12-20 10:10:17 +0100

    Update and rename selectRoute.m to MATLAB/selectRoute.m

--- MATLAB/selectRoute.m
+++ MATLAB/selectRoute.m
@@ -1,5 +1,28 @@
 function [name_figure,str_point,end_point,int_point] = selectRoute(nodes,links,detectors)
 
+%% Disclaimer
+% This file is part of the matlab package OpenTrafficCenter
+% developed by the KULeuven. 
+%
+% Copyright (C) 2018  Himpe Willem, Leuven, Belgium
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% More information at: https://github.com/HimpeWillem/OpenTrafficCenter
+% or contact: willem.himpe {@} kuleuven.be
+
+
 str_point=[];
 end_point=[];
 int_point=[];
