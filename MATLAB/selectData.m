bd44ebee55040992c1e4344e29f007d0d9c108e3
HimpeWillem <willem.himpe@gmail.com>
2018-12-20 10:09:38 +0100

    Update and rename selectData.m to MATLAB/selectData.m

--- MATLAB/selectData.m
+++ MATLAB/selectData.m
@@ -1,4 +1,28 @@
 function [id,lane,name,km_pos,time,speed_h,speed_m,flow_veh,flow_pae,occupancy,link_id,link_projection] = selectData(data,detectors)
+
+%% Disclaimer
+% This file is part of the matlab package OpenTrafficCenter
+% developed by the KULeuven. 
+%
+% Copyright (C) 2018  Himpe Willem, Leuven, Belgium
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% More information at: https://github.com/HimpeWillem/OpenTrafficCenter
+% or contact: willem.himpe {@} kuleuven.be
+
+
 %% Setup a nice format for the detectors allong the route
 global summer_time;
 
