44dbf74e758e3364640ede5a89da45e3a567bd69
HimpeWillem <willem.himpe@gmail.com>
2018-12-20 09:57:21 +0100

    Update VISUALIZECORRIDOR_NoDB.m

--- VISUALIZECORRIDOR_NoDB.m
+++ VISUALIZECORRIDOR_NoDB.m
@@ -2,10 +2,32 @@
 % Code developed for Athens Course at the KUL (November 2018)
 % Contact info: willem.himpe ([@]) kuleuven.be
 
+%% Disclaimer
+% This file is part of the matlab package OpenTrafficCenter
+% developed by the KULeuven. 
+%
+% Copyright (C) 2016  Himpe Willem, Leuven, Belgium
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% More information at: https://github.com/HimpeWillem/OpenTrafficCenter
+% or contact: willem.himpe {@} kuleuven.be
+
+%% Get the data
 % First go to http://db.itscrealab.be/download/loop_detectors/ and download
 % the data you want to visualize. Next extract the .csv files into the main
 % folder.
-
 
 %% Set a winter vs summer time boolean
 global summer_time;
