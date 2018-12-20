6b1fa7c61c30e02552f465382cdb389ab0b60155
HimpeWillem <willem.himpe@gmail.com>
2018-12-20 09:53:44 +0100

    Update CREATEMODEL.m

--- MATLAB/CREATEMODEL.m
+++ MATLAB/CREATEMODEL.m
@@ -1,3 +1,26 @@
+%% Disclaimer
+% This file is part of the matlab package OpenTrafficCenter
+% developed by the KULeuven. 
+%
+% Copyright (C) 2016  Himpe Willem, Leuven, Belgium
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% More information at: https://github.com/HimpeWillem/OpenTrafficCenter
+% or contact: willem.himpe {@} kuleuven.be
+
+%% Set the demand 
 % Add ODmatrices
 [timeSeries,ODmatrices] = createODs(links,origins,id,link_id,time,speed_h,flows_pae);
 
