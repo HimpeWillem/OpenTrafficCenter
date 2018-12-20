c7a680a5f7600eff1fd95a9c6548cb980b382f03
HimpeWillem <willem.himpe@gmail.com>
2018-12-20 09:56:38 +0100

    Update and rename VISUALIZECORRIDOR.m to MATLAB/VISUALIZECORRIDOR.m

--- MATLAB/VISUALIZECORRIDOR.m
+++ MATLAB/VISUALIZECORRIDOR.m
@@ -1,6 +1,27 @@
 %% Visualise Traffic Data allong a corridor or at a specific location
+
+%% Disclaimer
+% This file is part of the matlab package OpenTrafficCenter
+% developed by the KULeuven. 
+%
+% Copyright (C) 2018  Himpe Willem, Leuven, Belgium
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%
+% More information at: https://github.com/HimpeWillem/OpenTrafficCenter
+% or contact: willem.himpe {@} kuleuven.be
 % Code developed for Athens Course at the KUL (November 2018)
-% Contact info: willem.himpe ([@]) kuleuven.be
 
 % The data is downloaded from the opendata portal of the Flamish traffic 
 % centre (http://opendata.vlaanderen.be) and stored on a KUL server. This
